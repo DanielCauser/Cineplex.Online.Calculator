d51a5d3e0a6b7bc2b8a1070da4bfd3db7219dd24
Daniel John Causer <danielcauser@gmail.com>
2017-07-15 18:49:42 -0400

    Calculation done. App ready.

--- Cineplex.Online.Calculator/Models/ShippingContainerModel.cs
+++ Cineplex.Online.Calculator/Models/ShippingContainerModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+
 namespace Cineplex.Online.Calculator.Models
 {
     public class ShippingContainerModel
@@ -11,9 +13,6 @@ namespace Cineplex.Online.Calculator.Models
         public double Width { get; private set; }
         public double Height { get; private set; }
 
-        private double volumeOfCarton { get; set; }
-        private double volumeOfContainer { get; set; }
-
         public double AmountOfCartonsThatFitInContainer { get; private set; }
 
         public ShippingContainerModel(double length, double width, double height)
@@ -21,22 +20,31 @@ namespace Cineplex.Online.Calculator.Models
             Length = length;
             Width = width;
             Height = height;
+            CalculateAmountOfCartonsThatFitInContainer();
         }
 
         public void CalculateAmountOfCartonsThatFitInContainer()
         {
-            calculateVolumeOfCarton();
-            calculateVolumeOfContainer();
+            validateContainer();
+
+            AmountOfCartonsThatFitInContainer = 
+                (Length / LengthCarton) *
+                (Width / WidthCarton) *
+                (Height / HeightCarton);
         }
 
-        private void calculateVolumeOfCarton()
+        private void validateContainer()
         {
-            
+            StringBuilder errorMessage = new StringBuilder(string.Empty);
+            if (Length < LengthCarton)
+                errorMessage.AppendLine($"Container's lenght needs to be equal or greater than {LengthCarton}in.");
+			if (Width < WidthCarton)
+                errorMessage.AppendLine($"Container's width needs to be equal or greater than {WidthCarton}in.");
+			if (Height < HeightCarton)
+                errorMessage.AppendLine($"Container's height needs to be equal or greater than {HeightCarton}in.");
+
+            if (!string.IsNullOrEmpty(errorMessage.ToString()))
+                throw new Exception(errorMessage.ToString());
         }
-
-		private void calculateVolumeOfContainer()
-		{
-
-		}
     }
 }
--- Cineplex.Online.Calculator/ViewModels/OnlineCalculatorViewModel.cs
+++ Cineplex.Online.Calculator/ViewModels/OnlineCalculatorViewModel.cs
@@ -3,56 +3,78 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Cineplex.Online.Calculator.Models;
 using Xamarin.Forms;
 
 namespace Cineplex.Online.Calculator.ViewModels
 {
     public class OnlineCalculatorViewModel : BaseViewModel
     {
-        string length;
-		public string Length
-		{
-			get { return length; }
-			set
-			{
-				length = value;
-				OnPropertyChanged();
-			}
-		}
-		string width;
-		public string Width
-		{
-			get { return width; }
-			set
-			{
-				width = value;
-				OnPropertyChanged();
-			}
-		}
-        string height;
-		public string Height
-		{
-			get { return height; }
-			set
-			{
-				height = value;
-				OnPropertyChanged();
-			}
-		}
+        double length;
+        public string Length
+        {
+            get { return length == 0 ? string.Empty : length.ToString(); }
+            set
+            {
+                if (double.TryParse(value, out length))
+                {
+                    length = double.Parse(value);
+                    OnPropertyChanged();
+                }
+            }
+        }
+        double width;
+        public string Width
+        {
+            get { return width == 0 ? string.Empty : width.ToString(); }
+            set
+            {
+                if (double.TryParse(value, out width))
+                    OnPropertyChanged();
+            }
+        }
+        double height;
+        public string Height
+        {
+            get { return height == 0 ? string.Empty : height.ToString(); }
+            set
+            {
+                if (double.TryParse(value, out height))
+                    OnPropertyChanged();
+            }
+        }
+
+        double result;
+        public string Result
+        {
+            get { return result == 0 ? string.Empty : $"A total of {result} carton(s) fit in the container."; }
+            set
+            {
+                if (double.TryParse(value, out result))
+                    OnPropertyChanged();
+            }
+        }
 
         public ICommand CalculateCommand { get; set; }
 
         public OnlineCalculatorViewModel() : base("Shipping container calculation")
         {
-            CalculateCommand = new Command( async () =>
-            {
-                if (Busy) return;
-                Busy = true;
-
-                await Task.Delay(10000);
-
-                Busy = false;
-            });
+            CalculateCommand = new Command(async () =>
+           {
+               if (Busy) return;
+               Busy = true;
+               try
+               {
+                   var container = new ShippingContainerModel(length, width, height);
+                   Result = container.AmountOfCartonsThatFitInContainer.ToString();
+               }
+               catch (Exception ex)
+               {
+                    await App.Current.MainPage.DisplayAlert("Attention!", ex.Message, "OK");
+               }
+               
+               Busy = false;
+           });
         }
     }
 }
